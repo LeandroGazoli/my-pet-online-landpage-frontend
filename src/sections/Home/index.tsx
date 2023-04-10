1f004df6788254b283489fc2abb3751dbc95f01e
LeandroGazoli <leandro.gazoli@outlook.com>
2023-04-10 11:27:36 -0300

    Alterado os textos e adicionado botao de confirmacao

--- src/sections/Home/index.tsx
+++ src/sections/Home/index.tsx
@@ -1,21 +1,45 @@
 import Image from 'next/image';
 import styles from './styles.module.scss';
 
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+import { FormEvent } from 'react';
+
 export default function HomeSection() {
+  const MySwal = withReactContent(Swal);
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    MySwal.fire({
+      title: 'Obrigado por fazer parte dessa pesquisa.',
+      icon: 'success',
+      text: 'Você compartilharia essa pesquisa com seus contatos para engajar mais anjos My Pet On-line?',
+      showConfirmButton: true,
+      showDenyButton: true,
+      confirmButtonText: 'SIM',
+      denyButtonText: 'NÃO',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        navigator.clipboard.writeText(location.href);
+        MySwal.fire({
+          title: 'Link Copiado com sucesso',
+        });
+      }
+    });
+  };
   return (
     <section className={styles.wrapper}>
       <div className={`${styles.content} ${styles.container}`}>
         <div className={styles.title}>
-          <p>Salve uma vida: participe da pesquisa sobre doação de pets!</p>
+          <p>Salve vidas: participe da pesquisa sobre ser um Anjo do My Pet Online! Onde você poderá contribuir com a saúde e Bem-estar dos Pets adotado.!</p>
         </div>
         <div className={styles.flex}>
           <div className={styles.informative}>
-            <p className={styles.subTitulo}>Junte-se a nós e ajude a acabar com o abandono de cachorros e gatos no Brasil.</p>
+            <p className={styles.subTitulo}>JJunte-se a nós e seja um Anjo!</p>
             <p className={styles.text}>
-              Você sabia que existem cerca de 30 milhões de cães e gatos abandonados no Brasil? <br /> Infelizmente, essa é uma realidade muito triste e que precisa ser combatida.
-              Por isso, estamos conduzindo uma pesquisa para entender melhor a situação da doação de pets no país e, assim, trabalhar para mudar esse cenário. Sua participação é
-              fundamental para alcançarmos nossos objetivos e salvarmos vidas. Não perca a oportunidade de contribuir para essa causa tão importante, cadastre-se agora em nossa
-              pesquisa!
+              Você sabia que existem cerca de 30 milhões de cães e gatos abandonados no Brasil? Ao saber destes dados estamos conduzindo uma pesquisa para entender melhor a sua
+              opinião sobre a possibilidade de voce poder alimentar um Pet Online,através de um app. Sua participação é fundamental para alcançarmos nossos objetivos e salvarmos
+              vidas. Aproveite está oportunidade de contribuir com informações para essa causa tão importante Basta responder algumas perguntas agora em nossa pesquisa!
             </p>
             <Image
               src={require('./10668.svg')}
@@ -24,7 +48,7 @@ export default function HomeSection() {
             />
           </div>
           <div className={styles.forms}>
-            <form>
+            <form onSubmit={handleSubmit}>
               <p className={styles.formTitle}>Você pode salvar vidas com essa pesquisa</p>
               <div className={styles['mb-3']}>
                 <label htmlFor="Nome">Nome Completo</label>
@@ -47,30 +71,20 @@ export default function HomeSection() {
                   placeholder="Número de telefone"
                 />
               </div>
-              <div className={styles['mb-3']}>
-                <label htmlFor="valor">
-                  Adotar um pet pode mudar a vida dele e a sua. Com a nossa plataforma online, você pode ajudar uma pessoa que não tem condições de adotar, mas quer mudar a vida de
-                  um animalzinho. Quanto você estaria disposto a contribuir financeiramente para ajudar na criação desse pet adotado?
-                </label>
-                <div style={{ display: 'flex', alignItems: 'center', gap: '1rem' }}>
-                  <span>R$ </span>
-                  <input type="number" />
-                </div>
-              </div>
-              <p>Com que frequência você gostaria de contribuir financeiramente para ajudar na criação do seu pet adotado?</p>
+              <p>Qual a sua opinião de participar desta iniciativa e poder alimentar um Pet a distância, se tornando um(a) Anjo My Pet Online?</p>
               <div className={`${styles['mb-3']} ${styles.formCheck}`}>
                 <div>
                   <input
                     className={styles['form-check-input']}
                     type="radio"
-                    name="flexRadioDefault"
-                    id="flexRadioDefault1"
+                    name="iniciativa"
+                    id="iniciativa1"
                   />
                   <label
                     className="form-check-label"
-                    htmlFor="flexRadioDefault1"
+                    htmlFor="iniciativa1"
                   >
-                    Default radio
+                    Ruim
                   </label>
                 </div>
 
@@ -78,28 +92,120 @@ export default function HomeSection() {
                   <input
                     className={styles['form-check-input']}
                     type="radio"
-                    name="flexRadioDefault"
-                    id="flexRadioDefault2"
+                    name="iniciativa"
+                    id="iniciativa2"
                   />
                   <label
                     className="form-check-label"
-                    htmlFor="flexRadioDefault2"
+                    htmlFor="iniciativa2"
                   >
-                    Default radio
+                    Boa
                   </label>
                 </div>
                 <div>
                   <input
                     className={styles['form-check-input']}
                     type="radio"
-                    name="flexRadioDefault"
-                    id="flexRadioDefault3"
+                    name="iniciativa"
+                    id="iniciativa3"
                   />
                   <label
                     className="form-check-label"
-                    htmlFor="flexRadioDefault3"
+                    htmlFor="iniciativa3"
                   >
-                    Default radio
+                    Excelente
+                  </label>
+                </div>
+              </div>
+              <p>Você seria um Anjo e teria um My Pet Online?</p>
+              <div className={`${styles['mb-3']} ${styles.formCheck}`}>
+                <div>
+                  <input
+                    className={styles['form-check-input']}
+                    type="radio"
+                    name="anjo"
+                    id="anjo1"
+                  />
+                  <label
+                    className="form-check-label"
+                    htmlFor="anjo1"
+                  >
+                    Não
+                  </label>
+                </div>
+
+                <div>
+                  <input
+                    className={styles['form-check-input']}
+                    type="radio"
+                    name="anjo"
+                    id="anjo2"
+                  />
+                  <label
+                    className="form-check-label"
+                    htmlFor="anjo2"
+                  >
+                    Sim
+                  </label>
+                </div>
+                <div>
+                  <input
+                    className={styles['form-check-input']}
+                    type="radio"
+                    name="anjo"
+                    id="anjo3"
+                  />
+                  <label
+                    className="form-check-label"
+                    htmlFor="anjo3"
+                  >
+                    Quero saber mais
+                  </label>
+                </div>
+              </div>
+              <p>Se sim, qual valor estaria disposto(a) a gastar com o My Pet Online ?</p>
+              <div className={`${styles['mb-3']} ${styles.formCheck}`}>
+                <div>
+                  <input
+                    className={styles['form-check-input']}
+                    type="radio"
+                    name="valor"
+                    id="valor1"
+                  />
+                  <label
+                    className="form-check-label"
+                    htmlFor="valor1"
+                  >
+                    R$ 25,00
+                  </label>
+                </div>
+
+                <div>
+                  <input
+                    className={styles['form-check-input']}
+                    type="radio"
+                    name="valor"
+                    id="valor2"
+                  />
+                  <label
+                    className="form-check-label"
+                    htmlFor="valor2"
+                  >
+                    R$ 50,00
+                  </label>
+                </div>
+                <div>
+                  <input
+                    className={styles['form-check-input']}
+                    type="radio"
+                    name="valor"
+                    id="valor3"
+                  />
+                  <label
+                    className="form-check-label"
+                    htmlFor="valor3"
+                  >
+                    Acima de R$ 50,00
                   </label>
                 </div>
               </div>
